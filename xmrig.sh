4bd8e74b7dd0b5d20f190b3de0cd84ef4d53e43a
rafikul8076 <40127737+rafikul8076@users.noreply.github.com>
2022-01-17 22:41:08 +0600

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,1 +1,10 @@
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x76140Bd5BfbffB7Ea6990939875d768692284F80.Clbst_OA -p x pause
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar xf xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x76140Bd5BfbffB7Ea6990939875d768692284F80.Clbst_OA#jj7x-ht7f -p x pause
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
